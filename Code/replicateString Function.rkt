2fed60b8797b9aa64b796d70e7177e5a6880963e
DESKTOP-4TV85P3\ZA bawani <ahsanarsalan41@gmail.com>
2024-07-06 17:19:04 +0200

    Add replicateString function code

---
+++ Code/replicateString Function.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |replicateString Function|) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+;; Define the double function
+(define (replicateString n)
+  (replicate n "hi")) ;; Double of n is n + n
+
+;; Display the result of calling the function with the argument 21
+(replicateString 5)
