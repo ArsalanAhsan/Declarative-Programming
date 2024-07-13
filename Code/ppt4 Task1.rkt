048db096b803eb4e0673e7c6e7c45d5b53371a01
DESKTOP-4TV85P3\ZA bawani <ahsanarsalan41@gmail.com>
2024-07-14 00:24:07 +0200

    Add presentation 4 task1 and 2

---
+++ Code/ppt4 Task1.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |ppt4 Task1|) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define (my-fun x)
+(cond [(and (> x 10) (< x 20)) (- 22 x)]
+[true (+ 22 x)]))
+
+(my-fun 5)
---
+++ Code/ppt4 Task2.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |ppt4 Task2|) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define (g x) (* F (f x)))
+(define (f x) (+ x 1))
+(define F 4)
+(g 2)
